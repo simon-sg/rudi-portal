40838e269757b1f11fe1a76c37646e1a1536c277
Simon <armunzen@gmail.com>
2026-07-27 23:45:12 +0200

    feat(front): make producer name clickable in page heading
    The producer name shown in the page heading (dataset detail,
    selfdata detail pages) now links to the producer's organization page,
    keeping its original text color and underlining only on hover. The
    link is disabled on the organization page's own heading, since it
    shows the generic "Organisation" label there instead of a producer
    name.

--- rudi-application/rudi-application-front-office/angular-project/src/app/shared/core/layout/page-heading/page-heading.component.ts
+++ rudi-application/rudi-application-front-office/angular-project/src/app/shared/core/layout/page-heading/page-heading.component.ts
@@ -1,14 +1,17 @@
 import { NgClass } from '@angular/common';
 import {Component, Input} from '@angular/core';
 import {MatIcon} from '@angular/material/icon';
+import {RouterLink} from '@angular/router';
 import {BreakpointObserverService, MediaSize} from '@core/services/breakpoint-observer.service';
+import {URIComponentCodec} from '@core/services/codecs/uri-component-codec';
+import {TranslatePipe} from '@ngx-translate/core';
 import {OrganizationLogoComponent} from '../../../business/organisation/organization-logo/organization-logo.component';
 
 @Component({
     selector: 'app-page-heading',
     templateUrl: './page-heading.component.html',
     styleUrls: ['./page-heading.component.scss'],
-    imports: [NgClass, OrganizationLogoComponent, MatIcon]
+    imports: [NgClass, OrganizationLogoComponent, MatIcon, RouterLink, TranslatePipe]
 })
 export class PageHeadingComponent {
 
@@ -27,9 +30,15 @@ export class PageHeadingComponent {
     @Input()
     status: string;
 
+    @Input()
+    organizationClickable = true;
+
     mediaSize: MediaSize;
 
-    constructor(private readonly breakpointObserverService: BreakpointObserverService) {
+    constructor(
+        private readonly breakpointObserverService: BreakpointObserverService,
+        protected readonly uriComponentCodec: URIComponentCodec,
+    ) {
         this.mediaSize = this.breakpointObserverService.getMediaSize();
     }
 }
